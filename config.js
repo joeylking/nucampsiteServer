9d4ce1e0e2b85fac93316819467aad5b20d44e8e
Joey <30815220+joeylking@users.noreply.github.com>
2021-06-04 23:28:52 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '318720243301370',
+        clientSecret: '4a934d2f3a708a11be6e45d7f8e7fac1'
+    }
 }
